2c9cdcbe46a2179ceed92963c3e192e3fa8adae3
Yoni Yalovitsky <yoni.yalovitsky@gmail.com>
2012-06-05 13:16:18 +0300

    refactoring and added support for queries in specific db

--- lib/generators/project/features/steps/mysql_steps.rb
+++ lib/generators/project/features/steps/mysql_steps.rb
@@ -1,95 +1,39 @@
 require 'rubygems'
 require 'mysql'
 
-# simple functions to get at the number of currently running processes
-# on a mysql server
-def get_process_count()
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query('SHOW PROCESSLIST;')
-  @processes = results.size
-end
+AVERAGING_TIMEFRAME = 2
 
-# lots of helper funtions to retrieve information from the GLOBAL STATUS
-# table where MySQL stores information about it's current state
-def get_connection_count()
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query("SHOW GLOBAL STATUS where Variable_name = 'Connections';")
-  @connections = results.size
-end
-
-def get_threads_connected_count()
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query("SHOW GLOBAL STATUS where Variable_name = 'Threads_connected';")
-  @threads_connected = results.size
-end
-
-def get_threads_running_count()
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query("SHOW GLOBAL STATUS where Variable_name = 'Threads_running';")
-  @threads_running = results.size
-end
-
-def get_cached_queries_count()
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query("SHOW GLOBAL STATUS where Variable_name = 'Qcache_queries_in_cache';")
-  @cached_queries = results.size
-end
-
-def get_queries_per_second()
-  my = Mysql.connect(@host, @username, @password)
-  query = "SHOW GLOBAL STATUS where Variable_name = 'Com_update' or Variable_name = 'Com_select' or Variable_name = 'Com_delete' or Variable_name = 'Com_insert';"
-  results = my.query(query)
-  starting = results.fetch_row[1].to_i + results.fetch_row[1].to_i + results.fetch_row[1].to_i + results.fetch_row[1].to_i
-  sleep 10
-  results = my.query(query)
-  ending = results.fetch_row[1].to_i + results.fetch_row[1].to_i + results.fetch_row[1].to_i + results.fetch_row[1].to_i
-  # now calculate the difference and divide by 10
-  (ending.to_i-starting.to_i)/10
-end
-
-def get_results_per_second(query)
-  my = Mysql.connect(@host, @username, @password)
-  results = my.query(query)
-  starting = results.fetch_row[1]
-  sleep 10
-  results = my.query(query)
-  ending = results.fetch_row[1]
-  # now calculate the difference and divide by 10
-  (ending.to_i-starting.to_i)/10
-end
-
-def get_updates_per_second()
-  @updates_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Com_update';")
-end
-
-def get_selects_per_second()
-  @selects_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Com_select';")
-end
-
-def get_deletes_per_second()
-  @deletes_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Com_delete';")
-end
-
-def get_inserts_per_second()
-  @inserts_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Com_insert';")
-end
-
-def get_cache_hits_per_second()
-  @cache_hits_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Qcache_hots';")
-end
-
-def get_questions_per_second()
-  @questions_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Questions';")
-end
-
-def get_slow_queries_per_second()
-  @slow_queries_per_second = get_results_per_second("SHOW GLOBAL STATUS where Variable_name = 'Slow_queries';")
-end
-
-# specify the username and password if we're testing a default local install
 Before do
   @username = 'root'
   @password = ''
+  @database = nil
+end
+
+#Helper functions
+def run_query(query)
+  Mysql.connect(@host, @username, @password, @database).query(query)
+end
+
+def get_process_count()
+  run_query('SHOW PROCESSLIST;').num_rows
+end
+
+def get_table_count(table_like)
+  query = "select count(1) from information_schema.tables where table_schema = '#{@database}' and table_name like '#{table_like}';"
+  run_query(query).fetch_row[0].to_i
+end
+
+def get_global_status_sum(variables)
+  in_string = variables.map{ |str| "'#{str}'" }.join(", ")
+  query = "select sum(variable_value) as value from information_schema.global_status where variable_name in(#{in_string});" 
+  run_query(query).fetch_row[0].to_i
+end
+
+def count_global_status_per_second(variables)
+  starting = get_global_status_sum(variables) 
+  sleep AVERAGING_TIMEFRAME
+  ending = get_global_status_sum(variables) 
+  (ending.to_i-starting.to_i)/AVERAGING_TIMEFRAME
 end
 
 # Step definitions for testing the state of the MySQL server
@@ -105,132 +49,120 @@ And /I use the password (.+)$/ do |password|
   @password = password
 end
 
+And /I use the database (.+)$/ do |database|
+  @database = database
+end
+
+#schema tests:
+Then /^it should have the table ([^\'\"]+)$/ do |table|
+  get_table_count(table).should == 1
+end
+
+Then /^it should not have the table ([^\'\"]+)$/ do |table|
+  get_table_count(table).should == 0
+end
+
+#statistical tests:
 Then /it should have less than (\d+) processes$/ do |processes|
-  get_process_count()
-  @processes.should < processes.to_i
-end
-
-Then /it should have less than (\d+) connections$/ do |connections|
-  get_connection_count()
-  @connections.should < connections.to_i
-end
-
-Then /it should have less than (\d+) threads running$/ do |threads|
-  get_threads_running_count()
-  @threads_running.should < threads.to_i
-end
-
-Then /it should have less than (\d+) threads connected$/ do |threads|
-  get_threads_connected_count()
-  @threads_connected.should < threads.to_i
-end
-
-Then /it should have at least (\d+) queries cached$/ do |cached_queries|
-  get_cached_queries_count()
-  @cached_queries.should >= cached_queries.to_i
-end
-
-Then /it should have less than (\d+) update queries per second$/ do |queries|
-  get_updates_per_second()
-  @updates_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) insert queries per second$/ do |queries|
-  get_inserts_per_second()
-  @inserts_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) delete queries per second$/ do |queries|
-  get_deletes_per_second()
-  @deletes_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) selects queries per second$/ do |queries|
-  get_selects_per_second()
-  @selects_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) queries per second$/ do |queries|
-  get_queries_per_second()
-  @queries_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) cache hits per second$/ do |queries|
-  get_cache_hits_per_second()
-  @cache_hits_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) slow queries per second$/ do |queries|
-  get_slow_queries_per_second()
-  @slow_queries_per_second.should < queries.to_i
-end
-
-Then /it should have less than (\d+) questions per second$/ do |queries|
-  get_questions_per_second()
-  @questions_per_second.should < queries.to_i
+  get_process_count().should < processes.to_i
 end
 
 Then /it should have at least (\d+) processes$/ do |processes|
-  get_process_count()
-  @processes.should >= processes.to_i
+  get_process_count().should >= processes.to_i
+end
+
+Then /it should have less than (\d+) connections$/ do |connections|
+  get_global_status_sum(['Connections']).should < connections.to_i
 end
 
 Then /it should have at least (\d+) connections$/ do |connections|
-  get_connection_count()
-  @connections.should >= connections.to_i
+  get_global_status_sum(['Connections']).should >= connections.to_i
+end
+
+Then /it should have less than (\d+) threads running$/ do |threads|
+  get_global_status_sum(['Threads_running']).should < threads.to_i
 end
 
 Then /it should have at least (\d+) threads running$/ do |threads|
-  get_threads_running_count()
-  @threads_running.should >= threads.to_i
+  get_global_status_sum(['Threads_running']).should >= threads.to_i
+end
+
+Then /it should have less than (\d+) threads connected$/ do |threads|
+  get_global_status_sum(['Threads_connected']).should < threads.to_i
 end
 
 Then /it should have at least (\d+) threads connected$/ do |threads|
-  get_threads_connected_count()
-  @threads_connected.should >= threads.to_i
+  get_global_status_sum(['Threads_connected']).should >= threads.to_i
 end
 
 Then /it should have at least (\d+) queries cached$/ do |cached_queries|
-  get_cached_queries_count()
-  @cached_queries.should >= cached_queries.to_i
+  get_global_status_sum(['Qcache_queries_in_cache']).should >= cached_queries.to_i
+end
+
+Then /it should have at least (\d+) queries cached$/ do |cached_queries|
+  get_global_status_sum(['Qcache_queries_in_cache']).should >= cached_queries.to_i
+end
+
+Then /it should have less than (\d+) update queries per second$/ do |queries|
+  count_global_status_per_second(['Com_update']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) update queries per second$/ do |queries|
-  get_updates_per_second()
-  @updates_per_second.should >= queries.to_i
+  count_global_status_per_second(['Com_update']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) insert queries per second$/ do |queries|
+  count_global_status_per_second(['Com_insert']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) insert queries per second$/ do |queries|
-  get_inserts_per_second()
-  @inserts_per_second.should >= queries.to_i
+  count_global_status_per_second(['Com_insert']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) delete queries per second$/ do |queries|
+  count_global_status_per_second(['Com_delete']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) delete queries per second$/ do |queries|
-  get_deletes_per_second()
-  @deletes_per_second.should >= queries.to_i
+  count_global_status_per_second(['Com_delete']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) selects queries per second$/ do |queries|
+  count_global_status_per_second(['Com_select']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) selects queries per second$/ do |queries|
-  get_selects_per_second()
-  @selects_per_second.should >= queries.to_i
+  count_global_status_per_second(['Com_select']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) queries per second$/ do |queries|
+  count_global_status_per_second(['Com_update', 'Com_select', 'Com_delete', 'Com_insert']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) queries per second$/ do |queries|
-  get_queries_per_second()
-  @queries_per_second.should >= queries.to_i
+  count_global_status_per_second(['Com_update', 'Com_select', 'Com_delete', 'Com_insert']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) cache hits per second$/ do |queries|
+  count_global_status_per_second(['Qcache_hits']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) cache hits per second$/ do |queries|
-  get_cache_hits_per_second()
-  @cache_hits_per_second.should >= queries.to_i
+  count_global_status_per_second(['Qcache_hits']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) slow queries per second$/ do |queries|
+  count_global_status_per_second(['Slow_queries']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) slow queries per second$/ do |queries|
-  get_slow_queries_per_second()
-  @slow_queries_per_second.should >= queries.to_i
+  count_global_status_per_second(['Slow_queries']).should >= queries.to_i
+end
+
+Then /it should have less than (\d+) questions per second$/ do |queries|
+  count_global_status_per_second(['Questions']).should < queries.to_i
 end
 
 Then /it should have at least (\d+) questions per second$/ do |queries|
-  get_questions_per_second()
-  @questions_per_second.should >= queries.to_i
+  count_global_status_per_second(['Questions']).should >= queries.to_i
 end
